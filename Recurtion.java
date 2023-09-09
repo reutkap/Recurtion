c94b39c1b82ec7b063f6a2ba06518b6e08959042
Reutkap <126346601+reutkap@users.noreply.github.com>
2023-09-09 18:40:15 +0300

    Add files via upload

---
+++ Recurtion.java
@@ -0,0 +1,49 @@
+package homework;
+import java.util.*;
+public class Recurtion {
+	public static Scanner input= new Scanner (System.in);
+	public static int q1(int n) {
+		if (n==1) {
+			return n;
+		}
+		return q1(n-1)+n;
+	}
+	public static int q2(int n) {
+		if (n==1) {
+			return n;
+		}
+		return q2(n-1)*n;
+	}
+	public static int q3(int n) {
+		if(n%2==0) {
+			return q3(n-1);
+		}
+		return q3(n-2)*n;
+	}
+	public static int q4(int n) {
+		if(n==1) {
+			return n;
+		}
+		return q4(n/10)+1;
+	}
+	public static int q5 (int x, int y) {
+		if (y==0) {
+			return y;
+		}
+		return q5(x-y,y)+1;
+	}
+	public static boolean q9(int n) {
+		if ((n/10)<1) {
+			return true;
+		}
+		if ((n%10%2)==(n/10%10%2)) {
+			return q9 (n/10);
+		}
+		
+		return false;
+	}
+	public static void main(String[] args) {
+		System.out.println(q9(2));
+	}
+
+}
